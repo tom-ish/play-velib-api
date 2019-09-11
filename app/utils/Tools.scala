8fb6572e148aedd56858b538a5bd914a30d8d9f9
Tomohiro Ishiwata <tomohiro.ishiwata@gmail.com>
2019-09-11 18:03:22 +0200

    json pretty print

--- app/utils/Tools.scala
+++ app/utils/Tools.scala
@@ -11,7 +11,9 @@ object Tools {
     val records = (jsObject \ "records").get
     records match {
       case JsArray(recordsArray) =>
-        recordsArray.foreach(rslt += _.toString())
+        recordsArray.foreach {
+          rslt += mkVelibRecord(_)
+        }
 //        rslt += recordsArray.toString()
       case  _ => rslt += "error"
     }
@@ -22,6 +24,19 @@ object Tools {
 //      case JsError(errors) => rslt += errors.mkString
 //    }
 
+    rslt
+  }
+
+  def mkVelibRecord(jsValue: JsValue): String = {
+    var rslt = ""
+    jsValue match {
+      case JsNull => rslt += "null\n"
+      case boolean: JsBoolean =>rslt += boolean.toString() + "\n"
+      case JsNumber(value) => rslt += s"number => ${value.toString}\n"
+      case JsString(value) => rslt += s"string => ${value.toString}\n"
+      case JsArray(value) => rslt += s"jsArray => ${value.toString}\n"
+      case JsObject(value) => rslt += s"jsObject => ${value.toString}\n"
+    }
     rslt
   }
 }
