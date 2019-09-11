dbd86ede2afbc05cda2c539fc6dff5cbcaddca20
Tomohiro Ishiwata <tomohiro.ishiwata@gmail.com>
2019-09-11 17:20:13 +0200

    json pretty print

--- app/utils/Tools.scala
+++ app/utils/Tools.scala
@@ -3,14 +3,22 @@ package utils
 import play.api.libs.json._
 
 object Tools {
+  val jsonTransformer = (__ \ Symbol("records")).json.pick
+
   def mkHtml(jsObject: JsObject): String = {
     var rslt = ""
-    val records = (jsObject \ "records").transform((__ \ Symbol("records")).json.pick)
 
+    val records = (jsObject \ "records").get
     records match {
-      case JsSuccess(velibStation, path) => rslt += Json.prettyPrint(velibStation) + "\n"
-      case JsError(errors) => rslt += errors.mkString
+      case record: JsObject => rslt += Json.prettyPrint(record) + "\n"
+      case _ =>rslt += _
     }
+
+//    val records = (jsObject \ "records").transform(jsonTransformer)
+//    records match {
+//      case JsSuccess(velibStation, path) => rslt += Json.prettyPrint(velibStation) + "\n"
+//      case JsError(errors) => rslt += errors.mkString
+//    }
 
     rslt
   }
